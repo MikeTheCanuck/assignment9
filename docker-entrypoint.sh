38577cebee964d88664e0d710a5ab7e08ec36699
Mike Lonergan <mikethecanuck@gmail.com>
2017-03-02 17:45:01 -0800

    further troubleshooting - pulling getconfig.sh into Travis environment not Docker container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
-./bin/getconfig.sh
+# 2017-03-02 troubleshooting - since getconfig can't see Travis ENV VARs, should this even run here?
+# ./bin/getconfig.sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn homelessAPI.wsgi:application -b :8000
