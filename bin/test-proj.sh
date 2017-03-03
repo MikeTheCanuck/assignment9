d6ff9b6c76ce44f0ed15abbe8e90d7259321cd1d
Mike Lonergan <mikethecanuck@gmail.com>
2017-03-02 20:53:20 -0800

    removing the Django test until we get help from Zak

--- bin/test-proj.sh
+++ bin/test-proj.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 source ./bin/env.sh
-docker-compose run homeless-service python manage.py test
+
+# 2017-03-02 testing out this docker build command - don't know how to pass in env vars yet
+docker build -t mikethecanuck/homeless-service .
+# docker build -t $DOCKER_REPO/$DOCKER_IMAGE .
+# docker build -t "$DOCKER_REPO"/"$DOCKER_IMAGE" .
+
+# 2017-03-02 If the above docker build doesn't work, this might be an alternative
+# docker-compose build  
+
+# 2017-03-02 this isn't working correctly - Zak can't get to work in Docker environment either
+# docker-compose run homeless-service python manage.py test
